d6c6b30335f120fc2304e609c967e7d72dd4b8db
blazifex <blazifex@gmail.com>
2023-12-02 19:49:22 +1100

    cleanup

--- 2023/day02.py
+++ 2023/day02.py
@@ -7,12 +7,11 @@ with open("inputs/inp02.txt") as fh:
 p1, p2 = [], []
 r_min, g_min, b_min = 12, 13, 14
 
-for game in games:
-    d = {'id': re.search('\d+', game[0]).group(0), 'blue': 0, 'red': 0, 'green': 0}
-    game[1] = game[1].split('; ')
-    game[1] = [pull.split(', ') for pull in game[1]]
-    red, green, blue = [0], [0], [0]
-    for pull in game[1]:
+for game, pulls in games:
+    d = {'id': re.search('\d+', game).group(0), 'blue': 0, 'red': 0, 'green': 0}
+    pulls = [pull.split(', ') for pull in pulls.split('; ')]
+    red, green, blue = [], [], []
+    for pull in pulls:
         for cube in pull:
             if re.search('red', cube):
                 red.append(int(re.search('\d+', cube).group(0)))
@@ -24,7 +23,7 @@ for game in games:
     d['green'] = max(green)
     d['blue'] = max(blue)
     if d['red'] <= r_min and d['green'] <= g_min and d['blue'] <= b_min: p1.append(int(d['id']))
-    p2.append(int(d['blue'])*int(d['red'])*int(d['green']))
+    p2.append(d['blue']*d['red']*d['green'])
 
 print(sum(p1))
 print(sum(p2))
