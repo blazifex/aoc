4ba002df2acfa126200c0ca6e71b6dfd6894f940
blazifex <blazifex@gmail.com>
2023-12-03 19:44:58 +1100

    day 3 p2

--- 2023/day03.py
+++ 2023/day03.py
@@ -5,39 +5,59 @@ with open("inputs/inp03.txt") as fh:
 
 pattern = r'[^\d\.]'
 p1, p2 = [], []
+nums = []
+chars = []
+num_index = 0
+char_index = 0
 
 for line_id, line in enumerate(INPUT):
-    if re.findall('\d+', line):
-        nums = re.findall('\d+', line)
+    results = re.finditer('\d+', line)
+    for result in results:
+        d = {'id': num_index, 'val': result.group(), 'row': line_id, 'col_start': result.start(), 'col_end': result.end()-1}
+        num_index+=1
+        nums.append(d)
+    
+    results = re.finditer('[^\d\.]', line)
+    for result in results:
+        d = {'id': char_index, 'val': result.group(), 'row': line_id, 'col': result.start()}
+        char_index+=1
+        chars.append(d)
+
+for num in nums:
+    for char in chars:
+        if char['row'] == num['row']:
+            if num['col_start'] == char['col']+1: p1.append(int(num['val'])) #if char is just before num
+            if num['col_end'] == char['col']-1: p1.append(int(num['val'])) #if char is just after num
+        if char['row'] == num['row']+1:
+            if num['col_start']-1 <= char['col'] <= num['col_end']+1: p1.append(int(num['val'])) #if char is below number, incl. adjacent
+        if char['row'] == num['row']-1:
+            if num['col_start']-1 <= char['col'] <= num['col_end']+1: p1.append(int(num['val'])) #if char is above number, incl. adjacent
+
+for char in chars:
+    if char['val'] == '*': 
+        adj_count = 0
+        adj = []
         for num in nums:
-            num_only = r'(?<!\d)' + num + '(?!\d)'
-            loc = re.search(num_only, line).span()
-            for char_id, char in enumerate(line):
-                if loc[1]-1 >= char_id >= loc[0]: 
-                    if char_id > 0 and re.match(pattern, line[char_id-1]): #check characters before on same line
-                        p1.append(int(num))
-                        break
-                    if char_id < len(line)-1 and re.match(pattern, line[char_id+1]): #check characters after on same line
-                        p1.append(int(num))
-                        break
-                    if line_id > 0 and re.match(pattern, INPUT[line_id-1][char_id]): #check characters above
-                        p1.append(int(num))
-                        break
-                    if line_id < len(INPUT)-1 and re.match(pattern, INPUT[line_id+1][char_id]): #check characters below
-                        p1.append(int(num))
-                        break
-                    if line_id < len(INPUT)-1 and char_id > 0 and re.match(pattern, INPUT[line_id+1][char_id-1]): #check characters diagonally left and down
-                        p1.append(int(num))
-                        break
-                    if line_id < len(INPUT)-1 and char_id < len(line)-1 and re.match(pattern, INPUT[line_id+1][char_id+1]): #check characters diagonally right and down
-                        p1.append(int(num))
-                        break
-                    if line_id > 0 and char_id > 0 and re.match(pattern, INPUT[line_id-1][char_id-1]): #check characters diagonally left and up
-                        p1.append(int(num))
-                        break
-                    if line_id > 0 and char_id < len(line)-1 and re.match(pattern, INPUT[line_id-1][char_id+1]): #check characters diagonally right and up
-                        p1.append(int(num))
-                        break
-                
+            if char['row'] == num['row']:
+                if num['col_start'] == char['col']+1: #if char is just before num
+                    adj.append(int(num['val'])) 
+                    adj_count+=1
+                if num['col_end'] == char['col']-1: #if char is just after num
+                    adj.append(int(num['val'])) 
+                    adj_count+=1
+            if char['row'] == num['row']+1:
+                if num['col_start']-1 <= char['col'] <= num['col_end']+1: #if char is below number, incl. adjacent
+                    adj.append(int(num['val'])) 
+                    adj_count+=1
+            if char['row'] == num['row']-1:
+                if num['col_start']-1 <= char['col'] <= num['col_end']+1: #if char is above number, incl. adjacent
+                    adj.append(int(num['val'])) 
+                    adj_count+=1
+        if adj_count == 2:
+            result = 1
+            for num in adj:
+                result *= num
+            p2.append(result)
+
 print(sum(p1))
 print(sum(p2))
