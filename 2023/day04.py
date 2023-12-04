9f5dcc67e7d31aee5519134781bcd731eee147d4
blazifex <blazifex@gmail.com>
2023-12-04 17:04:54 +1100

    refactoring

--- 2023/day04.py
+++ 2023/day04.py
@@ -15,16 +15,19 @@ for id, line in enumerate(INPUT):
     
     win = re.findall(r'(\d+)', nums[0])
     have = re.findall(r'(\d+)', nums[1])
-    d = {'id': id+1, 'win': win, 'have': have}
-    cards.append(d)
-    
+    d = {'id': id+1, 'win': win, 'have': have, 'win_count': 0}
     val = 0
+    win_count = 0
+
     for h in have:
-        for w in win:
-            if h == w:
-                if val == 0: val = 1
-                else: val *= 2
+        if h in win:
+            win_count += 1
+            if val == 0: val = 1
+            else: val *= 2
+    
     p1+= val
+    d['win_count'] = win_count
+    cards.append(d)
 
 x = 0
 while x < len(cards):
@@ -32,17 +35,13 @@ while x < len(cards):
     owned.append(x)
 
 for id in owned:
-    for card in cards:
-        if card['id'] == id:
-            val = 0
-            for h in card['have']:
-                for w in card['win']:
-                    if h == w: val+=1
-            x = 1
-            while x <= val:
-                owned.append(id+x)
-                x+=1
+    val = cards[id-1]['win_count']
+    x = 1
+    while x <= val:
+        owned.append(id+x)
+        x+=1
 
-print(p1)
-print(len(owned))
+print(p1) #part 1
+print(len(owned)) #part 2
+
 print ('[Finished in {:.2f}ms]'.format(1000*(time.time() - startTime)))
