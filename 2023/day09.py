da13af15e5cf9194c76d2fb155672dfb52719bd5
blazifex <blazifex@gmail.com>
2023-12-09 17:19:26 +1100

    refactoring

--- 2023/day09.py
+++ 2023/day09.py
@@ -6,20 +6,17 @@ p1, p2 = [], []
 with open("inputs/inp09.txt") as fh:
     INPUT = fh.read().rstrip().split('\n')
 
-def get_next(vals, part):
+def get_next(l, rev): # reversed math for p2
     nxt = []
-    for id, val in enumerate(vals[1:], 1):
-        nxt.append(val - vals[id-1])
-    if nxt.count(0) != len(nxt): 
-        if part == 1: nxt.append(nxt[-1] + get_next(nxt, 1))
-        else: nxt.append(nxt[0] - get_next(nxt, 2))
-    else: nxt.append(0)
-    return nxt[-1]
+    for id, n in enumerate(l[1:], 1): nxt.append(n - l[id-1])    
+    if nxt.count(0) == len(nxt): return 0
+    if rev: return nxt[0] - get_next(nxt, 1) 
+    else: return nxt[-1] + get_next(nxt, 0)
 
 for line in INPUT:
     x = list(map(int, line.split()))
-    p1.append(x[-1] + get_next(x, 1))
-    p2.append(x[0] - get_next(x, 2))
+    p1.append(x[-1] + get_next(x, 0))
+    p2.append(x[0] - get_next(x, 1))
 
 print(sum(p1))
 print(sum(p2))
