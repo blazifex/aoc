262b8bb3f2809a0a9b3265ae1712b10b1fcd8e61
blazifex <95356713+blazifex@users.noreply.github.com>
2024-12-03 00:15:58 +1100

    Add files via upload

--- 2024/day02.py
+++ 2024/day02.py
@@ -4,29 +4,24 @@ with open("inp02.txt") as fh:
 
 p1, p2 = 0, 0
 
-def checkSafe(line: list):
-    for x in range(len(line)-1):
-        if not 1 <= (abs(int(line[x]) - int(line[x+1]))) <= 3: return False
-    return True
-
-def solve(line: list, ans = 0):
+def is_safe(line: list):
     if line == sorted(line) or line == sorted(line, reverse=True):
-        if checkSafe(line): return True
+        for x in range(len(line)-1):
+            if not 1 <= (abs(line[x] - line[x+1])) <= 3: return False
+        return True
     return False
 
 for line in lines:
     line = [int(x) for x in line.split()]
-    if solve(line): p1 += 1
+    x = copy.deepcopy(line)
+    x.pop(-1)
+    if is_safe(line): p1+=1
+    elif is_safe(x): p2+=1
     else:
-        for x in range(len(line)-1):
-            y = copy.deepcopy(line)
-            y.pop(-1)
-            if solve(y): 
-                p2+=1
-                break
+        for n in range(len(line)-1):
             z = copy.deepcopy(line)
-            z.pop(x)
-            if solve(z): 
+            z.pop(n)
+            if is_safe(z): 
                 p2+=1
                 break
 
