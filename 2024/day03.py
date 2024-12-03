d2ebf47eeef09912b290b01fd0c71baae1ec2985
blazifex <95356713+blazifex@users.noreply.github.com>
2024-12-03 21:43:21 +1100

    Add files via upload

--- 2024/day03.py
+++ 2024/day03.py
@@ -4,10 +4,9 @@ with open("inp03.txt") as fh:
     lines = fh.read().strip().split('\n')
 
 p1, p2 = 0, 0
-line = ''
 enabled = []
-
-for x in lines: line += x
+line = ''
+for x in lines: line += x #combine into a flat line
 
 def calc(line: str, ans = 0):
     for x in re.findall(r'mul\([0-9]+\,[0-9]+\)', line):
@@ -16,9 +15,8 @@ def calc(line: str, ans = 0):
     return ans
 
 p1 += calc(line)
-enabled += re.findall(r'(?=^|do\(\)).*?(?=don\'t\(\)|$)', line)
-for x in enabled:
-    p2 += calc(x)
+enabled += re.findall(r'(?=^|do\(\)).*?(?=don\'t\(\)|$)', line) #anything between start of line or do() up to either end of line or don't()
+for x in enabled: p2 += calc(x)
 
 print('P1: ', p1)
 print('P2: ', p2)
