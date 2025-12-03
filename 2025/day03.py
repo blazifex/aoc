ee1377ab5cb434838389a072869ec9f6c7c1051d
Nathan Forsyth <blazifex@pm.me>
2025-12-04 02:07:05 +1100

    day 3 updates

--- 2025/day03.py
+++ 2025/day03.py
@@ -6,43 +6,35 @@ p1, p2 = 0, 0
 with open("inputs/inp03.txt") as fh:
     input = fh.read().strip().split('\n')
 
-# Part 1
-for bank in input:
-    high = 0
-    for x in range(len(bank)):
-        for y in bank[x+1:]:
-            test = int(bank[x] + y)
-            if test > high:
-                high = test
-    p1 += high
+def calc(val):
+    ans = 0
+    for bank in input:
+        batt_amt = val
+        complete = False
+        left = 0
+        active = ''
 
-# Part 2
-for bank in input:
-    complete = False
-    batt_amt = 12
-    left = 0
-
-    while batt_amt > 0:
-        if batt_amt == len(bank[left:]):
-            active += bank[left:]
-            complete = True
-        if not complete:
-            max_jolts = 0
-            right = left+len(bank)-batt_amt
-            active = ''
-            bracket = bank[left:right+1]
-            for x in range(len(bracket)):
-                curr_jolts = int(bracket[x])
-                if curr_jolts > max_jolts:
-                    max_jolts = curr_jolts
-                    max_pos = x
-            active += str(max_jolts)
-            left += max_pos+1
+        while batt_amt > 0:
+            if batt_amt == len(bank[left:]):
+                active += bank[left:]
+                complete = True
+            if not complete:
+                right = len(bank)-batt_amt
+                bracket = bank[left:right+1]
+                max_jolts = 0
+                max_pos = 0
+                for x in range(0, len(bracket)):
+                    curr_jolts = int(bracket[x])
+                    if curr_jolts > max_jolts:
+                        max_jolts = curr_jolts
+                        max_pos = x
+                active += str(max_jolts)
+                left += max_pos+1
             batt_amt -= 1
+        ans += int(active)
+    return ans
 
-    p2+=int(active)
-    
-# Outputs
-print("P1: ", p1)
-print("P2: ", p2)
+print("P1: ", calc(2))
+print("P2: ", calc(12))
+
 print ('[Finished in {:.2f}ms]'.format(1000*(time.time() - startTime)))
