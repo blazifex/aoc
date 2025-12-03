1812b80be28f14145d3899699e09e7c19589ea97
Nathan Forsyth <blazifex@pm.me>
2025-12-04 02:38:02 +1100

    refactoring, cleaned up variable names

--- 2025/day03.py
+++ 2025/day03.py
@@ -1,40 +1,43 @@
 import time
 
-startTime = time.time()
+start_time = time.time()
 p1, p2 = 0, 0
 
 with open("inputs/inp03.txt") as fh:
     input = fh.read().strip().split('\n')
 
-def calc(val):
-    ans = 0
+def calc(n):
+    total = 0
     for bank in input:
-        batt_amt = val
+        batteries_needed = n
         complete = False
         left = 0
         active = ''
 
-        while batt_amt > 0:
-            if batt_amt == len(bank[left:]):
+        while batteries_needed > 0 and not complete:
+            right = len(bank) + 1 - batteries_needed
+            bracket = bank[left:right]
+            high_jolts = 0
+            high_pos = 0
+
+            for x in range(0, len(bracket)):
+                current_jolts = int(bracket[x])
+                if current_jolts > high_jolts:
+                    high_jolts = current_jolts
+                    high_pos = x
+
+            active += str(high_jolts)
+            left += high_pos + 1
+            batteries_needed -= 1
+
+            if batteries_needed == len(bank[left:]):
                 active += bank[left:]
                 complete = True
-            if not complete:
-                right = len(bank)-batt_amt
-                bracket = bank[left:right+1]
-                max_jolts = 0
-                max_pos = 0
-                for x in range(0, len(bracket)):
-                    curr_jolts = int(bracket[x])
-                    if curr_jolts > max_jolts:
-                        max_jolts = curr_jolts
-                        max_pos = x
-                active += str(max_jolts)
-                left += max_pos+1
-            batt_amt -= 1
-        ans += int(active)
-    return ans
+
+        total += int(active)
+
+    return total
 
 print("P1: ", calc(2))
 print("P2: ", calc(12))
-
-print ('[Finished in {:.2f}ms]'.format(1000*(time.time() - startTime)))
+print ('[Finished in {:.2f}ms]'.format(1000*(time.time() - start_time)))
