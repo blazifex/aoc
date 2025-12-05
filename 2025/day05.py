3c7eb2521431b6132e704b5caa5de9ec8a06be07
Nathan Forsyth <njforsyth@pm.me>
2025-12-06 02:40:05 +1100

    variable name cleanup

--- 2025/day05.py
+++ 2025/day05.py
@@ -6,29 +6,29 @@ p1, p2 = 0, 0
 with open("inputs/inp05.txt") as fh:
     input = fh.read().strip().split('\n\n')
 
-id_sets = [list(map(int, line.split('-'))) for line in input[0].split('\n')]
-ingredients = input[1].split('\n')
+fresh_ranges = [list(map(int, line.split('-'))) for line in input[0].split('\n')]
+ingredient_ids = list(map(int, input[1].split('\n')))
 
-for ingredient in ingredients:
+for ingredient_id in ingredient_ids:
     spoiled = True
-    for id_set in id_sets:
-        if int(ingredient) >= int(id_set[0]) and int(ingredient) <= int(id_set[1]) and spoiled: 
+    for fresh_range in fresh_ranges:
+        if ingredient_id >= fresh_range[0] and ingredient_id <= fresh_range[1] and spoiled: 
             spoiled = False
     if not spoiled: p1 += 1
 
-fresh_ids = []
-id_sets.sort(key=lambda x: x[0])
+merged_ranges = []
+fresh_ranges.sort(key=lambda x: x[0])
 
-for id_set in id_sets:
+for fresh_range in fresh_ranges:
     integrated = False
-    for n, fresh_id in enumerate(fresh_ids):
-        if id_set[0] <= fresh_id[1]+1 and not integrated:
-            fresh_ids[n][1] = max(fresh_id[1], id_set[1])
+    for n, merged_range in enumerate(merged_ranges):
+        if fresh_range[0] <= merged_range[1]+1 and not integrated:
+            merged_ranges[n][1] = max(merged_range[1], fresh_range[1])
             integrated = True
-    if not integrated: fresh_ids.append(id_set)
+    if not integrated: merged_ranges.append(fresh_range)
 
-for fresh_id in fresh_ids:
-    p2 += int(fresh_id[1]) - int(fresh_id[0]) + 1
+for merged_range in merged_ranges:
+    p2 += int(merged_range[1]) - int(merged_range[0]) + 1
 
 print("P1: ", p1)
 print("P2: ", p2)
