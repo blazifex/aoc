934f96c4abad350155ffc131c91ca345630bb295
Nathan Forsyth <njforsyth@pm.me>
2025-12-06 02:34:26 +1100

    day 5 updates

--- 2025/day05.py
+++ 2025/day05.py
@@ -6,14 +6,7 @@ p1, p2 = 0, 0
 with open("inputs/inp05.txt") as fh:
     input = fh.read().strip().split('\n\n')
 
-id_sets = input[0].split('\n')
-
-for id,id_set in enumerate(id_sets):
-    id_sets[id] = id_set.split('-')
-    id_sets[id][0] = int(id_sets[id][0])
-    id_sets[id][1] = int(id_sets[id][1])
-
-
+id_sets = [list(map(int, line.split('-'))) for line in input[0].split('\n')]
 ingredients = input[1].split('\n')
 
 for ingredient in ingredients:
@@ -26,24 +19,13 @@ for ingredient in ingredients:
 fresh_ids = []
 id_sets.sort(key=lambda x: x[0])
 
-#populate combined fresh ids
 for id_set in id_sets:
     integrated = False
     for n, fresh_id in enumerate(fresh_ids):
         if id_set[0] <= fresh_id[1]+1 and not integrated:
             fresh_ids[n][1] = max(fresh_id[1], id_set[1])
             integrated = True
-        if id_set[0] == fresh_id[0]:
-            if id_set[1] > fresh_id[1]:
-                fresh_ids[n][1] = id_set[1]
-            integrated = True
-
     if not integrated: fresh_ids.append(id_set)
-
-for x in fresh_ids:
-    print(x)
-
-
 
 for fresh_id in fresh_ids:
     p2 += int(fresh_id[1]) - int(fresh_id[0]) + 1
