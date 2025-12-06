112693e3c125c80f8c3893a767cbcacc84612ae3
Nathan Forsyth <njforsyth@pm.me>
2025-12-06 23:08:04 +1100

    day 4 refactoring

--- 2025/day04.py
+++ 2025/day04.py
@@ -12,6 +12,8 @@ for r, row in enumerate(input):
         row_list.append(char)
     input[r] = row_list
 
+neighbour_checks = [(1, 0), (1, 1), (1,-1), (-1, 0), (-1, 1), (-1,-1), (0,-1), (0, 1)]
+
 def is_valid(row, col):
     if len(input)-1 >= row >= 0 and len(input[0])-1 >= col >= 0: return True
     return False
@@ -22,22 +24,10 @@ def remove_rolls():
         for c, col in enumerate(row):
             if col != '.':
                 neighbours = 0
-                if is_valid(r+1, c): #1
-                    if input[r+1][c] != '.': neighbours += 1
-                if is_valid(r+1, c+1): #2
-                    if input[r+1][c+1] != '.': neighbours += 1
-                if is_valid(r+1, c-1): #3
-                    if input[r+1][c-1] != '.': neighbours += 1
-                if is_valid(r-1, c): #4
-                    if input[r-1][c] != '.': neighbours += 1
-                if is_valid(r-1, c+1): #5
-                    if input[r-1][c+1] != '.': neighbours += 1
-                if is_valid(r-1, c-1): #6
-                    if input[r-1][c-1] != '.': neighbours += 1
-                if is_valid(r, c-1): #7
-                    if input[r][c-1] != '.': neighbours += 1
-                if is_valid(r, c+1): #8
-                    if input[r][c+1] != '.': neighbours += 1
+                for dr, dc in neighbour_checks:
+                    nr, nc = r + dr, c + dc
+                    if is_valid(nr, nc) and input[nr][nc] != '.':
+                        neighbours += 1
                 if neighbours < 4: 
                     input[r][c] = 'x'
                     n += 1
