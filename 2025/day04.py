853276f999a56f1b04352d008d00987ed9baa4ba
Nathan Forsyth <njforsyth@pm.me>
2025-12-06 22:57:47 +1100

    day 4 complete

--- 2025/day04.py
+++ 2025/day04.py
@@ -16,37 +16,49 @@ def is_valid(row, col):
     if len(input)-1 >= row >= 0 and len(input[0])-1 >= col >= 0: return True
     return False
 
-for r, row in enumerate(input):
-    for c, col in enumerate(row):
-        if col != '.':
-            neighbours = 0
-            # | +1, 0 | +1, +1 | +1, -1 | -1, 0 | -1, +1 | -1, -1 | 0, -1 | 0, +1 |
-            if is_valid(r+1, c): #1
-                if input[r+1][c] != '.': neighbours += 1
-            if is_valid(r+1, c+1): #2
-                if input[r+1][c+1] != '.': neighbours += 1
-            if is_valid(r+1, c-1): #3
-                if input[r+1][c-1] != '.': neighbours += 1
-            if is_valid(r-1, c): #4
-                if input[r-1][c] != '.': neighbours += 1
-            if is_valid(r-1, c+1): #5
-                if input[r-1][c+1] != '.': neighbours += 1
-            if is_valid(r-1, c-1): #6
-                if input[r-1][c-1] != '.': neighbours += 1
-            if is_valid(r, c-1): #7
-                if input[r][c-1] != '.': neighbours += 1
-            if is_valid(r, c+1): #8
-                if input[r][c+1] != '.': neighbours += 1
-            if neighbours < 4: 
-                input[r][c] = 'x'
-                p1 += 1
+def remove_rolls():
+    n = 0
+    for r, row in enumerate(input):
+        for c, col in enumerate(row):
+            if col != '.':
+                neighbours = 0
+                if is_valid(r+1, c): #1
+                    if input[r+1][c] != '.': neighbours += 1
+                if is_valid(r+1, c+1): #2
+                    if input[r+1][c+1] != '.': neighbours += 1
+                if is_valid(r+1, c-1): #3
+                    if input[r+1][c-1] != '.': neighbours += 1
+                if is_valid(r-1, c): #4
+                    if input[r-1][c] != '.': neighbours += 1
+                if is_valid(r-1, c+1): #5
+                    if input[r-1][c+1] != '.': neighbours += 1
+                if is_valid(r-1, c-1): #6
+                    if input[r-1][c-1] != '.': neighbours += 1
+                if is_valid(r, c-1): #7
+                    if input[r][c-1] != '.': neighbours += 1
+                if is_valid(r, c+1): #8
+                    if input[r][c+1] != '.': neighbours += 1
+                if neighbours < 4: 
+                    input[r][c] = 'x'
+                    n += 1
+    return n
 
-"""for row in input:
-    row_string = ''
-    for col in row:
-        row_string += col
-    print(row_string)"""
-    
+def refresh_input():
+    changed = False
+    for r, row in enumerate(input):
+        for c, col in enumerate(row):
+            if col == 'x': 
+                input[r][c] = '.'
+                changed = True
+    return changed
+
+p1 += remove_rolls()
+
+p2 = p1
+changed = True
+while changed:
+    changed = refresh_input()
+    p2 += remove_rolls()
 
 print("P1: ", p1)
 print("P2: ", p2)
