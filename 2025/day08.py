1f874972e9a4cb341f3c754f03dcae94cac54a89
Nathan Forsyth <blazifex@pm.me>
2025-12-09 22:13:38 +1100

    day 8 updates

--- 2025/day08.py
+++ 2025/day08.py
@@ -24,66 +24,47 @@ for x in range(len(junctions)-1):
             "distance": calc_distance(junctions[x], junctions[y])
             })
 
-#sort pairs by minimum distance
 pairs.sort(key=lambda x: x["distance"])
 
-circuits = [[pairs[0]["a"], pairs[0]["b"]]]
-x = 0
+def make_circuits(pairs):
+    circuits = []
+    final_a = None
+    final_b = None
 
-for pair in pairs[1:1000]:
-    a_circuit = None
-    b_circuit = None
-    for circuit in circuits:
-            if pair["a"] in circuit: a_circuit = circuit
-            if pair["b"] in circuit: b_circuit = circuit
+    for p, pair in enumerate(pairs):
+        a_circuit = None
+        b_circuit = None
+        for circuit in circuits:
+                if pair["a"] in circuit: a_circuit = circuit
+                if pair["b"] in circuit: b_circuit = circuit
+        if a_circuit and b_circuit:
+            if a_circuit != b_circuit:
+                a_circuit += [x for x in b_circuit if x not in a_circuit]
+                circuits.remove(b_circuit)
+        elif a_circuit:
+            if pair["b"] not in a_circuit:
+                a_circuit.append(pair["b"])
+                final_a = pair["a"]
+                final_b = pair["b"]
+        elif b_circuit:
+            if pair["a"] not in b_circuit:
+                b_circuit.append(pair["a"])
+                final_a = pair["a"]
+                final_b = pair["b"]
+        else:
+            circuits.append([pair["a"], pair["b"]])
+        
+    
+    return circuits, final_a, final_b
 
-    if a_circuit and b_circuit:
-        if a_circuit != b_circuit:
-            # merge b_circuit into a_circuit
-            a_circuit += [x for x in b_circuit if x not in a_circuit]
-            circuits.remove(b_circuit)
-    elif a_circuit:
-        if pair["b"] not in a_circuit:
-            a_circuit.append(pair["b"])
-    elif b_circuit:
-        if pair["a"] not in b_circuit:
-            b_circuit.append(pair["a"])
-    else:
-        circuits.append([pair["a"], pair["b"]])
+p1_circuits = make_circuits(pairs[:1000])[0]
 
-circuits.sort(key=len, reverse=True)
-for circuit in circuits[:3]:
+p1_circuits.sort(key=len, reverse=True)
+
+for circuit in p1_circuits[:3]:
     p1 *= len(circuit)
 
-
-circuits2 = [[pairs[0]["a"], pairs[0]["b"]]]
-final_a = None
-final_b = None
-
-for pair in pairs:
-    a_circuit = None
-    b_circuit = None
-    for circuit in circuits2:
-            if pair["a"] in circuit: a_circuit = circuit
-            if pair["b"] in circuit: b_circuit = circuit
-
-    if a_circuit and b_circuit:
-        if a_circuit != b_circuit:
-            # merge b_circuit into a_circuit
-            a_circuit += [x for x in b_circuit if x not in a_circuit]
-            circuits2.remove(b_circuit)
-    elif a_circuit:
-        if pair["b"] not in a_circuit:
-            a_circuit.append(pair["b"])
-            final_a = pair["a"]
-            final_b = pair["b"]
-    elif b_circuit:
-        if pair["a"] not in b_circuit:
-            b_circuit.append(pair["a"])
-            final_a = pair["a"]
-            final_b = pair["b"]
-    else:
-        circuits2.append([pair["a"], pair["b"]])
+final_a, final_b = make_circuits(pairs)[1:]
 
 p2 = final_a[0] * final_b[0]
 
